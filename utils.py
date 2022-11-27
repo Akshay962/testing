ddac28e97dc134bbde8e09391708153b17bf5d5e
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-27 06:58:02 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -360,7 +360,7 @@ def humanbytes(size):
 
 #link converter 
 
-shortzy = shortzy(SHORTENER_API, "shareus.in")
+shortzy = Shortzy(SHORTENER_API, "shareus.in")
 async def get_shortlink(link):
     if SHORTENER_API:
         if LONG_DROPLINK_URL == "True" or LONG_DROPLINK_URL is True:
