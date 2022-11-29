7906b7cc144fe0a2249f754ad5354ede0fc09fce
Akshay962 <107416405+Akshay962@users.noreply.github.com>
2022-11-29 23:50:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -49,7 +49,7 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action(enums.ChatAction.TYPING)
         m=await message.reply_sticker("CAACAgIAAxkBAAEcBR9jhEG3sdT1IdJ93CF-os1EBZTgRwAChBEAApFmcUiMQebkgC5X-x4E")
-        await asyncio.sleep(2)
+        await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
@@ -99,7 +99,7 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action(enums.ChatAction.TYPING)
         m=await message.reply_sticker("CAACAgIAAxkBAAEcBR9jhEG3sdT1IdJ93CF-os1EBZTgRwAChBEAApFmcUiMQebkgC5X-x4E")
-        await asyncio.sleep(2)
+        await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
